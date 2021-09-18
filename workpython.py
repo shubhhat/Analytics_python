72863a3cecd44e3567383811dc347b3eb40996d2
shubhhat <90966058+shubhhat@users.noreply.github.com>
2021-09-18 18:04:05 +0530

    intro program_0

---
+++ workpython.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+Aug_batch = [1,2,3,4,5,6,7,8]
+print(Aug_batch)
+Aug_batch
+import matplotlib.pyplot as pt
+pt.plot(Aug_batch)
